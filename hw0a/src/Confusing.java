0940495fbd4b46a98286bcb1ecd72c858132140c
HawardHuang <haward_huang@berkeley.edu>
2025-01-24 16:36:06 -0800

    Update Confusing.java

--- hw0a/src/Confusing.java
+++ hw0a/src/Confusing.java
@@ -29,7 +29,17 @@ public class Confusing {
 }
 
 /* Before running the code, type your answer below.
-
+I am method 1.
+I am method 1.
+I am method 2.
+I am method 3.
+I am method 1.
+I am method 1.
+I am method 2.
+I am method 1.
+I am method 2.
+I am method 3.
+I am method 1.
 TODO: Write output here
 
 Then, click the green play button to check your work. */
