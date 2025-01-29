2ccd748db9d0b317e63a5b10f9f856e1676077d1
HawardHuang <haward_huang@berkeley.edu>
2025-01-29 15:31:18 -0800

    Update MapExercises.java

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,26 +1,51 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MapExercises {
+
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        Map<Character, Integer> letterMap = new HashMap<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            letterMap.put(c, c - 'a' + 1);
+        }
+        return letterMap;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> squareMap = new HashMap<>();
+        for (int num : nums) {
+            squareMap.put(num, num * num);
+        }
+        return squareMap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+        return wordCount;
+    }
+
+    public static void main(String[] args) {
+        // Test letterToNum
+        Map<Character, Integer> letterMap = letterToNum();
+        System.out.println("Letter to Number (a to z): " + letterMap);
+
+        // Test squares
+        List<Integer> numbers = List.of(1, 3, 6, 7);
+        System.out.println("Squares: " + squares(numbers));
+
+        // Test countWords
+        List<String> words = List.of("apple", "banana", "apple", "cherry", "banana", "banana");
+        System.out.println("Word Counts: " + countWords(words));
     }
 }
