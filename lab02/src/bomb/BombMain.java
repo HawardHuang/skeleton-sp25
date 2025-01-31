8cb368e83f1a702dcfc3f554fb406395b4175d6d
HawardHuang <haward_huang@berkeley.edu>
2025-01-30 23:54:58 -0800

    Add files via upload
    finish lab02

---
+++ lab02/src/bomb/BombMain.java
@@ -0,0 +1,24 @@
+package bomb;
+
+import common.IntList;
+
+public class BombMain {
+    public static void answers(String[] args) {
+        int phase = 2;
+        if (args.length > 0) {
+            phase = Integer.parseInt(args[0]);
+        }
+        // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
+        Bomb b = new Bomb();
+        if (phase >= 0) {
+            b.phase0(String.valueOf(39291226));
+        }
+        if (phase >= 1) {
+            IntList lst = IntList.of(0,9,3,0,8);
+            b.phase1(lst); // Figure this out too
+        }
+        if (phase >= 2) {
+            b.phase2(String.valueOf(793227803));
+        }
+    }
+}
